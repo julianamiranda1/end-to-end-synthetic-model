cbdfdab71356dcf85c8c05023ee9286721e5e30c
Juliana Soares de Miranda <juliana.soares15434@gmail.com>
2026-04-06 22:48:53 -0300

    Ajustando init

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,15 @@ from .pratos import PratoInput, PratoOutput, DisponibilidadeInput
 from .bebidas import BebidaInput, BebidaOutput
 from .pedidos import PedidoInput, PedidoOutput
 from .reservas import ReservaInput, ReservaOutput
+
+__all__ = [
+    "PratoInput",
+    "PratoOutput",
+    "DisponibilidadeInput",
+    "BebidaInput",
+    "BebidaOutput",
+    "PedidoInput",
+    "PedidoOutput",
+    "ReservaInput",
+    "ReservaOutput",
+]
