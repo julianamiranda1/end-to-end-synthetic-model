4d96c2e857b30d020d7d6ff057be934a2226d31b
Juliana Soares de Miranda <juliana.soares15434@gmail.com>
2026-06-26 23:47:46 -0300

    Fix Black formatting

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
 import pytest
 from fastapi.testclient import TestClient
+
 from main import app
+
 
 @pytest.fixture
 def client():
-    """Fixture que fornece um cliente de teste para a aplicação FastAPI"""
+    """Fixture que fornece um cliente de teste para a aplicação FastAPI."""
     return TestClient(app)
